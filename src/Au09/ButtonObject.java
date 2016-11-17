ea0ecd1726a324e394b163138259c9c0fc4bc7e3
Jan Langela <Jan Langela@10.0.105.9>
2016-11-17 11:37:57 +0100

    commit

--- src/Au09/ButtonObject.java
+++ src/Au09/ButtonObject.java
@@ -3,10 +3,22 @@
  */
 package Au09;
 
+import javax.swing.*;
+
 /**
  * @author Jan Langela
  *
  */
 public class ButtonObject {
-
+	private JButton knopf;
+	private int x;
+	private int y;
+	private boolean active;
+	
+	public ButtonObject(int x, int y, boolean active){
+		this.knopf = new JButton();
+		this.x = x;
+		this.y = y;
+		this.active = active;
+	}
 }
