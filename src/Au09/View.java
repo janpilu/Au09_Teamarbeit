f5827ed84212709b12b715f39ee2eb3f050cec47
Armin <Armin@10.0.104.148>
2016-11-17 11:28:15 +0100

    Views

--- src/Au09/View.java
+++ src/Au09/View.java
@@ -11,33 +11,8 @@ public class View extends JFrame{
     private Controller c;
 //    private drawPanel d;
 
+    private JButton[] Button;
     
-    
-    public JButton Button1;
-    public JButton Button2;
-    public JButton Button3;
-    public JButton Button4;
-    public JButton Button5;
-    public JButton Button6;
-    public JButton Button7;
-    public JButton Button8;
-    public JButton Button9;
-    public JButton Button10;
-    public JButton Button11;
-    public JButton Button12;
-    public JButton Button13;
-    public JButton Button14;
-    public JButton Button15;
-    public JButton Button16;
-    public JButton Button17;
-    public JButton Button18;
-    public JButton Button19;
-    public JButton Button20;
-    public JButton Button21;
-    public JButton Button22;
-    public JButton Button23;
-    public JButton Button24;
-    public JButton Button25;
     
     
     
@@ -45,32 +20,7 @@ public class View extends JFrame{
     public View(Model m, Controller c){
         super();
         
-        this.Button1 = new JButton();
-        this.Button2 = new JButton();
-        this.Button3 = new JButton();
-        this.Button4 = new JButton();
-        this.Button5 = new JButton();
-        this.Button6 = new JButton();
-        this.Button7 = new JButton();
-        this.Button8 = new JButton();
-        this.Button9 = new JButton();
-        this.Button10 = new JButton();
-        this.Button11 = new JButton();
-        this.Button12 = new JButton();
-        this.Button13 = new JButton();
-        this.Button14 = new JButton();
-        this.Button15 = new JButton();
-        this.Button16 = new JButton();
-        this.Button17 = new JButton();
-        this.Button18 = new JButton();
-        this.Button19 = new JButton();
-        this.Button20 = new JButton();
-        this.Button21 = new JButton();
-        this.Button22 = new JButton();
-        this.Button23 = new JButton();
-        this.Button24 = new JButton();
-        this.Button25 = new JButton();
-        
+        this.Button = new JButton[25];
         this.m = m;
         this.c = c;
 
@@ -78,8 +28,9 @@ public class View extends JFrame{
         
         this.setLayout(new GridLayout(0,5));
      
-        
-        
+        for(int i = 0; i < 25; i++){
+        	this.add(Button[i]);
+        }
         
         
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -88,31 +39,6 @@ public class View extends JFrame{
 
         this.setSize(400, 400);
 
-        this.add(this.Button1);
-        this.add(this.Button2);
-        this.add(this.Button3);
-        this.add(this.Button4);
-        this.add(this.Button5);
-        this.add(this.Button6);
-        this.add(this.Button7);
-        this.add(this.Button8);
-        this.add(this.Button9);
-        this.add(this.Button10);
-        this.add(this.Button11);
-        this.add(this.Button12);
-        this.add(this.Button13);
-        this.add(this.Button14);
-        this.add(this.Button15);
-        this.add(this.Button16);
-        this.add(this.Button17);
-        this.add(this.Button18);
-        this.add(this.Button19);
-        this.add(this.Button20);
-        this.add(this.Button21);
-        this.add(this.Button22);
-        this.add(this.Button23);
-        this.add(this.Button24);
-        this.add(this.Button25);
 
         this.setVisible(true);
     }
