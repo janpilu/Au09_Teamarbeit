09f67a459b08cc18cef691a59250ac3ebb0ee0eb
Admin <Admin@10.0.104.39>
2016-11-22 15:27:25 +0100

    model boolean 2dim

--- src/Au09/Model.java
+++ src/Au09/Model.java
@@ -6,11 +6,19 @@ package Au09;
  * @author Simon Appel 
  * @version 2016-11-17
  */
-import java.util.ArrayList;
+
 
 public class Model {
-	private ArrayList buttonlist;
+	private boolean[][] check;
+
 	public Model(){
-		buttonlist = new ArrayList(25);
+		this.check = new boolean[7][7];
+	}
+	public void changeColor(){
+		if(this.v.check==true){	//schwarz ist
+			this.v.		//einfärben gelb
+		}else{
+			this.v.		//schwarz setzten
+		}
 	}
 }
