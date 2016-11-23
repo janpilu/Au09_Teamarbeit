d365927599b5323e2e0d9d0b459276c967b5a6b0
Armin <Armin@10.0.0.8>
2016-11-23 17:10:46 +0100

    wfihiwgf

--- src/Au09/Controller.java
+++ src/Au09/Controller.java
@@ -24,6 +24,5 @@ public class Controller implements ActionListener{
 		// TODO Auto-generated method stub
         System.out.println("Klicken");
         this.v.compare((JButton)e.getSource());
-        this.v.geloest();
 	}
 }
