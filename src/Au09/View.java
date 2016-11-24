12ec36f970869b2c31e87beb4f905d35a321b71c
Armin <Armin@10.0.0.8>
2016-11-24 09:38:50 +0100

    aaaaaa

--- src/Au09/View.java
+++ src/Au09/View.java
@@ -116,7 +116,8 @@ public class View extends JFrame{
     					this.m.checken(i-1,j);
     					this.m.checken(i,j-1);
     					this.m.checken(i,j+1);
-    					this.m.checken(i+1,j);					
+    					this.m.checken(i+1,j);	
+    					
     				}
     			}
     		}
